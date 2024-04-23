2045f669497dfef28e4f7afe06f623dd3279d87e
Matheus Siqueira <matheusbatista.dev@gmail.com>
2024-04-23 15:08:52 -0300

    Fix - Adiciona validacao de obrigatoriedade e unicidade em Tag name

---
+++ db/migrate/20240423175244_change_tag_name_required.rb
@@ -0,0 +1,5 @@
+class ChangeTagNameRequired < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :tags, :name, false 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_23_173549) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_23_175244) do
   create_table "contents", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_23_173549) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
